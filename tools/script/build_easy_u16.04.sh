13acc3195a71576fc84a8af802a7390a684fab78
Prashant K. Jha <pjha.sci@gmail.com>
2019-06-26 20:01:47 -0500

    updated script for ubuntu 16.04

--- tools/script/build_easy_u16.04.sh
+++ tools/script/build_easy_u16.04.sh
@@ -1,7 +1,8 @@
-#
-# export paths
-#
+# local directory with external libraries
 local="/home/prashant/Softwares/local_pd"
+
+# clean previous build files (optional)
+./clean.sh
 
 # hpx
 hpx_ver="1.1.0"
@@ -10,19 +11,13 @@ hpx_dir="$local""/hpx/hpx-""$hpx_ver"
 # yaml-cpp
 yamlcpp_ver="0.5.3"
 yamlcpp_dir="$local""/yaml-cpp/yaml-cpp-""$yamlcpp_ver"
-export PATH=$PATH:"$yamlcpp_dir"
-export PATH=$PATH:"$yamlcpp_dir""/include"
-export PATH=$PATH:"$yamlcpp_dir""/lib"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$yamlcpp_dir""/lib"
 
-# vtk
+# vtk (specify cmake folder inside vtk library)
 vtk_ver="8.0.1"
-vtk_dir="$local""/vtk/vtk-""$vtk_ver"
-export PATH=$PATH:"$vtk_dir"
-export PATH=$PATH:"$vtk_dir""/include"
-export PATH=$PATH:"$vtk_dir""/lib"
-export PATH=$PATH:"$vtk_dir""/bin"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$vtk_dir""/lib"
+vtk_dir="$local""/vtk/vtk-""$vtk_ver""/lib/cmake/vtk-8.0"
+
+# blaze
+blaze_dir="$local""/blaze"
 
 #
 #	target where we want to build PeridynamicsHPX
@@ -31,14 +26,15 @@ PWD=$pwd
 target_build="$PWD"
 
 # source
-source="../."
+source="../NLMech"
 
 #
 # run cmake with flags
 #
 cmake -DHPX_DIR="$hpx_dir""/lib/cmake/HPX" \
-		-DVTK_DIR="$vtk_dir" \
- 	   -DYAML_CPP_DIR="yamlcpp_dir" \
+		 -DVTK_DIR="$vtk_dir" \
+ 	   -DYAML_CPP_DIR="$yamlcpp_dir" \
+ 	   -DBLAZE_DIR="$blaze_dir" \
 	   -DCMAKE_INSTALL_PREFIX="$target_build" \
 	   -DCMAKE_BUILD_TYPE=Release \
 	   "$source"
