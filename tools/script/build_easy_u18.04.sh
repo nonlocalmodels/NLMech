ae7a3071858c63f0affbc619fef664fc38fa0425
Prashant K. Jha <pjha.sci@gmail.com>
2019-06-26 19:36:00 -0500

    updated compile script to include DBLAZE_DIR

--- tools/script/build_easy_u18.04.sh
+++ tools/script/build_easy_u18.04.sh
@@ -3,6 +3,8 @@
 #
 local="/home/prashant/Softwares/local"
 
+./clean.sh
+
 # hpx
 hpx_ver="1.1.0"
 hpx_dir="$local""/hpx/hpx-""$hpx_ver"
@@ -10,25 +12,25 @@ hpx_dir="$local""/hpx/hpx-""$hpx_ver"
 # yaml-cpp
 yamlcpp_ver="0.5.3"
 yamlcpp_dir="$local""/yaml-cpp/yaml-cpp-""$yamlcpp_ver"
-export PATH=$PATH:"$yamlcpp_dir"
-export PATH=$PATH:"$yamlcpp_dir""/include"
-export PATH=$PATH:"$yamlcpp_dir""/lib"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$yamlcpp_dir""/lib"
+
+# blaze
+blaze_dir="$local""/blaze"
 
 #
-#	target where we want to build PeridynamicsHPX
+# target where we want to build PeridynamicsHPX
 #
 PWD=$pwd
 target_build="$PWD"
 
 # source
-source="../."
+source="../NLMech-v0.1"
 
 #
 # run cmake with flags
 #
 cmake -DHPX_DIR="$hpx_dir""/lib/cmake/HPX" \
- 	  -DYAML_CPP_DIR="yamlcpp_dir" \
+ 	  -DYAML_CPP_DIR="$yamlcpp_dir" \
+ 	  -DBLAZE_DIR="$blaze_dir" \
 	  -DCMAKE_INSTALL_PREFIX="$target_build" \
 	  -DCMAKE_BUILD_TYPE=Release \
 	  "$source"
