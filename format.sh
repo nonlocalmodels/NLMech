5d245e8551e27e35b275d0fc44935bb455b329be
Patrick Diehl <patrickdiehl@lsu.edu>
2019-12-06 16:18:36 -0600

    Do not sort headers

--- format.sh
+++ format.sh
@@ -12,7 +12,7 @@ if [ ! -f ".clang-format" ]; then
 
 	echo "Generate clang format configuration file"
 	clang-format -style=Google -dump-config > .clang-format 
-    sed -i 's/SortIncludes:    true/SortIncludes:    false/g' ./clang-format
+    sed -i 's/SortIncludes:    true/SortIncludes:    false/g' .clang-format
 fi
 
 echo "Formating header files"
