2ec48be8e33ae6d2dbc719881f167e4923265a68
Patrick Diehl <patrickdiehl@lsu.edu>
2020-01-28 16:32:39 -0600

    Update year in copyright

--- tools/uScale/src/srcInclude.h
+++ tools/uScale/src/srcInclude.h
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  Copyright (c) 2019 Prashant K. Jha
-//  Copyright (c) 2019 Patrick Diehl
+//  Copyright (c) 2020 Prashant K. Jha
+//  Copyright (c) 2020 Patrick Diehl
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
