49a62a908e97c07b95ef5567f513162f467f50f8
Patrick <patrickdiehl@lsu.edu>
2020-12-07 19:45:59 -0600

    Update FindCmake

--- cmake/FindGmsh.cmake
+++ cmake/FindGmsh.cmake
@@ -6,9 +6,9 @@
 find_package(PkgConfig)
 
 find_library(GMSH_LIB
-        NAMES gmsh
-        PATH /usr/lib64 /usr/local/lib64 /usr/lib/ /usr/local/lib "${GMSH_DIR}/lib/"
-	PATH_SUFFIXES lib lib64)
+        NAMES libgmsh.a libgmsh.so
+        HINTS /usr/lib64 /usr/local/lib64 /usr/lib/ /usr/local/lib "${GMSH_DIR}"
+	    PATH_SUFFIXES lib lib64)
 find_path(GMSH_INCLUDE gmsh.h HINTS /usr/include /usr/local/include "${GMSH_DIR}/include/")
 
 mark_as_advanced(GMSH_DIR)
@@ -16,7 +16,7 @@ mark_as_advanced(GMSH_LIB)
 mark_as_advanced(GMSH_INCLUDE)
 
 if (NOT GMSH_LIB)
-    message(FATAL_ERROR "Gmsh Library not found: Specify the GMSH_DIR where yaml cpp is located")
+    message(FATAL_ERROR "Gmsh Library not found: Specify the GMSH_DIR where Gmsh is located")
 else ()
     include_directories(${GMSH_INCLUDE})
 endif ()
